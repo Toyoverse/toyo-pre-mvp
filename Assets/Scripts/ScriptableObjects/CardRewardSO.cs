a82e7fd0a381c9f74559ad285db367c207e4278d
Jhone Marra <jhonemetal.marra@gmail.com>
2022-08-10 18:45:27 -0300

    CardRewardSO update;

--- Assets/Scripts/ScriptableObjects/CardRewardSO.cs
+++ Assets/Scripts/ScriptableObjects/CardRewardSO.cs
@@ -6,6 +6,9 @@ public class CardRewardSO : ScriptableObject
     public int id;
     public string cardName;
     public string description;
+    public string memory;
     public CARD_TYPE cardType;
     public Sprite cardImage;
+    public ToyoPersonaSO toyoPersona;
+    public TrainingActionSO[] correctCombination;
 }
