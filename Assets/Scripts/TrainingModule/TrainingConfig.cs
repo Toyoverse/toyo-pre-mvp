58608a42114a52d1ba8d2daca586caed109b2faa
Jhone Marra <jhonemetal.marra@gmail.com>
2022-09-20 14:34:22 -0300

    Check Toyo training startAt for get event title in RewardScreen; BoxReward template for Xeon Pier0 box;

--- Assets/Scripts/TrainingModule/TrainingConfig.cs
+++ Assets/Scripts/TrainingModule/TrainingConfig.cs
@@ -45,6 +45,7 @@ public class TrainingConfig : Singleton<TrainingConfig>
     [HideInInspector] public string eventStory;
     [HideInInspector] public string sendToyoToTrainingPopUp;
     [HideInInspector] public string selectActionsMessage = "Select your actions...";
+    private string _trainingEventEndMessage = "This event has now ended.";
 
     //Default phrases for reward description
     [HideInInspector] public string rewardTitle = "TRAINING RESULTS";
@@ -577,6 +578,14 @@ public class TrainingConfig : Singleton<TrainingConfig>
 
         Print.Log("CardReward not fond - ID: " + id);
         return null;
+    }
+    
+    public string GetEventNameByActualTrainingId()
+    {
+        var _result = eventTitle;
+        if (Instance.GetCurrentTrainingInfo().startAt < startEventTimeStamp)
+            _result = _trainingEventEndMessage;
+        return _result;
     }
 }
 
